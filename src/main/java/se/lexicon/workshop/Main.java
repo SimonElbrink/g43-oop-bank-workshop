b37d09f3a70d92614158d27cfbf342b57b06af6c
Simon Elbrink <simon.elbrink@lexicon.se>
2022-07-04 11:16:21 +0200

    CustomerDAO - Done!

--- src/main/java/se/lexicon/workshop/Main.java
+++ src/main/java/se/lexicon/workshop/Main.java
@@ -1,9 +1,33 @@
 package se.lexicon.workshop;
 
+
+import se.lexicon.workshop.dao.CustomerDAO;
 import se.lexicon.workshop.model.Customer;
 
 public class Main {
     public static void main(String[] args) {
+
+        CustomerDAO customerDAO = new CustomerDAO();
+        boolean wasAdded = customerDAO.add("Peter","Chilling", "peter.chilling@mail.com");
+        boolean wasAddedDup = customerDAO.add("Peter","Chilling", "peter.chilling@mail.com");
+
+        customerDAO.add("Mimi", "Chilling", "mimi.chilling@mail.com");
+
+        customerDAO.add("Test", "Testsson", "test.testsson@mail.com");
+
+        Customer toEdit = customerDAO.findByCustomerId(9003);
+        toEdit.setEmail("simon@lexicon.se");
+
+
+
+        System.out.println("wasAdded = " + wasAdded);
+        System.out.println("wasAddedDup = " + wasAddedDup);
+
+        boolean wasRemoved = customerDAO.remove(9002);
+
+        Customer byCustomerId = customerDAO.findByCustomerId(9002);
+        System.out.println("byCustomerId = " + byCustomerId);
+
 
 
     }
--- src/main/java/se/lexicon/workshop/dao/CustomerDAO.java
+++ src/main/java/se/lexicon/workshop/dao/CustomerDAO.java
@@ -1,4 +1,59 @@
 package se.lexicon.workshop.dao;
 
+import se.lexicon.workshop.model.Customer;
+
+import java.util.ArrayList;
+
 public class CustomerDAO {
+    private ArrayList<Customer> customers = new ArrayList<>();
+
+    public boolean add(String firstName, String lastName, String email){
+
+        //Check if email is already stored.
+        for (Customer c : customers) {
+            if (c.getEmail().equals(email)){
+                System.out.println("Email have already been used");
+                return false;
+            }
+        }
+
+        // Adding to storage
+        Customer ctoAdd = new Customer(firstName, lastName, email);
+        customers.add(ctoAdd);
+
+        return true;
+
+    }
+
+    public Customer find(Customer customer){
+        if (customers.contains(customer)){
+            return customer;
+        }
+        return null;
+    }
+
+    public Customer findByCustomerId(int id){
+
+        for (Customer customer: customers){
+            if (customer.getId() == id){
+                return customer;
+            }
+        }
+
+        return null;
+    }
+
+    public boolean remove(Customer customer){
+        return customers.remove(customer);
+    }
+
+    public boolean remove(int customerId){
+        //Find customer by id
+        Customer foundCustomer = findByCustomerId(customerId);
+        //Call and return result of remove.
+        return remove(foundCustomer);
+    }
+
+
+
 }
