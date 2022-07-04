72e07701919e9e3bee8ba91f036277d1a3055c92
Simon Elbrink <simon.elbrink@lexicon.se>
2022-07-04 11:37:30 +0200

    BankAccount - DONE! minus tests.

--- src/main/java/se/lexicon/workshop/Main.java
+++ src/main/java/se/lexicon/workshop/Main.java
@@ -2,10 +2,11 @@ package se.lexicon.workshop;
 
 
 import se.lexicon.workshop.dao.CustomerDAO;
+import se.lexicon.workshop.model.BankAccount;
 import se.lexicon.workshop.model.Customer;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         CustomerDAO customerDAO = new CustomerDAO();
         boolean wasAdded = customerDAO.add("Peter","Chilling", "peter.chilling@mail.com");
@@ -27,6 +28,28 @@ public class Main {
 
         Customer byCustomerId = customerDAO.findByCustomerId(9002);
         System.out.println("byCustomerId = " + byCustomerId);
+
+
+        //---------
+
+        Customer c1 = customerDAO.findByCustomerId(9001);
+
+        BankAccount b1 = new BankAccount(50_000,c1);
+
+        System.out.println("b1 Account= " + b1);
+
+        b1.withdraw(50000);
+
+        System.out.println(b1.getBalance()); //100_000
+
+
+
+
+
+
+
+
+
 
 
 
--- src/main/java/se/lexicon/workshop/model/BankAccount.java
+++ src/main/java/se/lexicon/workshop/model/BankAccount.java
@@ -1,4 +1,71 @@
 package se.lexicon.workshop.model;
 
 public class BankAccount {
+
+    private static int bankAccountIDSequencer = 60;
+
+    public static int getCurrentBankAccountIDSequencer() {
+        return bankAccountIDSequencer;
+    }
+
+    private int id;
+    private double balance;
+    private Customer owner;
+
+    public BankAccount(double balance, Customer owner) {
+        this.id = ++bankAccountIDSequencer;
+        this.balance = balance;
+        this.owner = owner;
+    }
+
+    public void withdraw(double amount){
+        if (amount < 0) {
+            throw new IllegalArgumentException("Amount was negative");
+        }
+
+        if (amount > this.balance){
+            throw new IllegalArgumentException("Amount is to big, not sufficient funds.");
+        }
+
+        this.balance = this.balance - amount;
+    }
+
+    public void deposit(double amount){
+
+        if (amount < 0) {
+            throw new IllegalArgumentException("Amount was negative");
+        }
+
+        this.balance = this.balance + amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public Customer getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Customer owner) {
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return "BankAccount{" +
+                "id=" + id +
+                ", balance=" + balance +
+                ", owner=" + owner +
+                '}';
+    }
 }
